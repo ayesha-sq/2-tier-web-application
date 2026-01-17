3074527f65761d912a1c3fcf7571292c1286f5a6
ayesha-sq <ayesha.asq7@example.com>
2026-01-17 13:47:00 +0530

    Finalize MySQL config + retry for Docker

--- app.py
+++ app.py
@@ -7,11 +7,10 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
 
 # ---------------- MYSQL CONFIG ----------------
-# These values come from environment variables
 app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST', 'arcora-mysql')
 app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER', 'root')
 app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD', '')
-app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB', 'arcora_db')
+app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB', 'devops')
 
 mysql = MySQL(app)
 
@@ -65,4 +64,5 @@ def contact():
 if __name__ == "__main__":
     init_db()
     app.run(host="0.0.0.0", port=5000, debug=True)
+
 
